aabe592101633244890b6b5dc06b6e02da7782f1
EvertonHenriqueG <evertoninfon@gmail.com>
2025-10-31 22:52:35 -0300

    Atualização de teste se esta a funcionar

--- src/application/ProgramDateTimeFormat.java
+++ src/application/ProgramDateTimeFormat.java
@@ -1,4 +1,27 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.time.Instant;
+
 public class ProgramDateTimeFormat {
+    public static void main(String[] args) {
+        LocalDate d01 = LocalDate.parse("2022-07-20");
+        LocalDateTime d02 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d03 = Instant.parse("2022-07-20T01:30:26Z");
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        //pode ser assim
+        System.out.println("d01 = " + d01.format(fmt1));
+        //assim
+        System.out.println("d01 = " + fmt1.format(d01));
+        //assim
+        System.out.println("d01 = " + d01.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+
+    }
+    
+
 }
---
+++ src/enumeracaoComposicao/entitiesenums/OrderStatus.java
@@ -0,0 +1,8 @@
+package enumeracaoComposicao.entitiesenums;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
